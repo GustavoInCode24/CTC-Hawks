969676d81e23c38f8a2482f04fecd7aacf51fa4b
unknown <lucashnevessousa@gmail.com>
2025-11-25 20:08:32 -0300

    feat: criação de superuser e model pagina

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Pagina
 # Register your models here.
+
+admin.site.register(Pagina)
+
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
+class Pagina(models.Model):
+    # O verbose_name não é o dado do site, ele é apenas a "Etiqueta" (Rótulo) do campo que vai aparecer para você lá no Painel Administrativo.
+
+    nome_do_site = models.CharField(max_length=100, verbose_name="Nome do Site")
+    logo_do_site = models.ImageField(upload_to='logos/', verbose_name="Logo do Site", validators=[FileExtensionValidator(['pdf', 'svg', 'png', 'jpg', 'jpeg'])])
+    texto_chamada = models.TextField(verbose_name="Texto da Chamada (Hero)")
+    texto_sobre = models.TextField(verbose_name="Texto Sobre Nós")
+    imagem_sobre = models.ImageField(upload_to='sobre/', verbose_name="Imagem da Seção Sobre")
+
+    endereco = models.TextField(verbose_name="Endereço Completo")
+    email = models.EmailField(verbose_name="E-mail de Contato")
+    whatsapp = models.CharField(max_length=20, verbose_name="Número do WhatsApp")
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome_do_site
+
+    class Meta:
+        verbose_name = "Configuração da Página"
+        verbose_name_plural = "Configurações da Página"
