41dc6dab6fb52675351985285b2aa56c66ad8edf
GustavoinCode24 <gugaprogramador24@gmail.com>
2025-11-27 13:20:06 -0300

    subindo a configuração de arquivos estaticos

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root_coletados')
+
 STATICFILES_DIRS = [
-    BASE_DIR /'staticFiles'
+  os.path.join(BASE_DIR, 'staticFiles')
 ]
 
 # Default primary key field type
